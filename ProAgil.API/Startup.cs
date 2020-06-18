7bf1b899f5f51154789aa8b281d83fa0f25039e4
Amauri Cunha <proamauri@gmail.com>
2020-06-17 21:01:53 -0300

    Alteração

--- ProAgil.API/Startup.cs
+++ ProAgil.API/Startup.cs
@@ -31,6 +31,7 @@ namespace ProAgil.API
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,7 +44,7 @@ namespace ProAgil.API
             }
 
             //app.UseHttpsRedirection();
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
 
             app.UseAuthorization();
